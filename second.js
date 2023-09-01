3ab8e63dd07ce23aebf42bd9b22ed29c3ad11b21
SachidanandSharma2162 <114774479+SachidanandSharma2162@users.noreply.github.com>
2023-09-01 15:40:01 +0000

    date and time

---
+++ second.js
@@ -0,0 +1,34 @@
+
+// Date and Time in javascript
+const mydate=new Date()
+// console.log(mydate);
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toString());
+// console.log(mydate.toString().split(' '));
+
+// console.log(typeof(mydate));
+const newdate=new Date(2023,0,24,5,3)
+// 0 means january
+// console.log(newdate.toLocaleString());
+
+let timestamp=Date.now()
+// milisecond
+// console.log(timestamp);
+// console.log(newdate.getTime());
+// // convert into second
+// console.log(Math.floor(timestamp/1000));
+
+
+let date=new Date()
+
+// console.log(date);
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getMonth()+1);
+
+
+
+
