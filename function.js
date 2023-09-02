7b9b03d0bb83807017a0918857dacedf82e8ede7
SachidanandSharma2162 <114774479+SachidanandSharma2162@users.noreply.github.com>
2023-09-02 15:52:49 +0000

    functionday1

---
+++ function.js
@@ -0,0 +1,15 @@
+// function in js
+
+function add_two(a,b)//parameters/formal arguments
+{
+    let result=a+b
+    return result
+}
+// console.log(add_two(3,4));//actual arguments
+
+
+function remainder(a=num1,b=6){
+    return a%b
+}
+console.log((remainder(10,7)));
+
