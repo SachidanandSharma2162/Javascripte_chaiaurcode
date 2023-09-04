025ecafbf74c828dea948aa717762b6732d9a3f0
SachidanandSharma2162 <114774479+SachidanandSharma2162@users.noreply.github.com>
2023-09-04 05:56:13 +0000

    iffe completed

--- iife.js
+++ iife.js
@@ -6,19 +6,43 @@
 
 (function chai(){
     // normal function/named iife
-    console.log("Welcome!");
+    // console.log("Welcome!");
 })();
 
 
 (()=>{
     //arraow function
     // unnamed iife
-    console.log("WELCOME!");
+    // console.log("WELCOME!");
 })();
 
 
 // passing variables
 ((name)=>{
     //arraow function
-    console.log(`hello! ${name}`);
+    // console.log(`hello! ${name}`);
 })("Sachin");
+
+
+const user={
+    name:"ADITYA",
+    age:23,
+    email:"adityaverma@gmail.com",
+    welcome:function yoo(){
+        console.log(this.age);
+    }
+    // console.log(this);
+}
+
+// console.log(user.welcome());
+
+
+let val1=10
+let val2=5
+// const addtwo=(num1,num2)=>(num1+num2);
+function addtwo(num1,num2){
+    return num1+num2
+}
+// console.log(addtwo(val1,val2));
+// console.log(addtwo(5,2));
+
