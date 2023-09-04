7b5755859988f5e51c8073cd439afce7c5e5f11b
SachidanandSharma2162 <114774479+SachidanandSharma2162@users.noreply.github.com>
2023-09-04 12:26:55 +0000

    loops

---
+++ loops.js
@@ -0,0 +1,63 @@
+// for loop
+
+// const array=[1,2,3,4,5,6]
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    // console.log(element);
+// }
+
+// nesting of loops
+
+// for (let i = 1; i <=10; i++) {
+//     // const element = array[i];
+    // console.log("table of " + i + " is");
+//     for (let j = 1; j <=10; j++) {
+//         // const element = array[j];
+        // console.log(i + "*" + j + "=" + i*j);   
+//     }   
+// }
+
+// break statement: takes flow out of the loop
+// for (let i = 1; i <=10; i++) {
+//     // const element = array[i];
+//     if(i==5)
+//        break;
+//     else
+//         console.log(i);
+// }
+
+// continue statement
+// takes control in next iteration
+// for (let i = 1; i <=10; i++) {
+//     // const element = array[i];
+//     if(i==5)
+//        continue;
+//     else
+//         console.log(i);
+// }
+
+// while(condition){} loop
+
+let index=0
+
+// while (index<=10) {
+    // console.log(index);
+//     index+=1
+// }
+
+
+const arr=["Batman","Joker","Flash","Superman"]
+
+// while (index<arr.length) {
+//     console.log(arr[index++]);
+// }
+
+// do while() loop
+// do while loop runs for once without checking the condition
+
+do {
+    console.log(arr[index++]);
+} while (index<arr.length);
+
+// Loops on array
+
